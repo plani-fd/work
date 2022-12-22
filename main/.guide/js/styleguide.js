7443c5ed64601c88e8641946e3e38ab2ccac0f6c
조민지 <iam924mj@plani.co.kr>
2022-12-22 11:19:03 +0900

    스타일 가이드

--- main/.guide/js/styleguide.js
+++ main/.guide/js/styleguide.js
@@ -1,10 +1,37 @@
 $(function()
-{
-    $('main > .section > .group').each(function(i)
+{   
+    $('main > .section').each(function(i)
     {
-        $(this).find('> pre').text( $(this).find('> .item').html() );
+        // 아이디값 넣기
+        $('main > .section').eq(i).attr('id', 'section' + i);
+
+        // 메뉴 생성
+        $('header nav').append('<a href="#section' + i + '">' + $(this).find(' > h2').text() + '</a>');
+
+        // 마크업 한 번 넣어도 소스 저절로 생성되게
+        $('main > .section').eq(i).find('> .group').each(function(j)
+        {
+            $(this).find('> pre').text( $(this).find('> .item').html() );
+        });
+    });
+
+    // 메뉴 눌렀을 때
+    $('header nav a').on('click', function()
+    {
+        $(this).siblings('a').removeClass('active');
+        $(this).addClass('active');
+
+        $('html, body').animate({scrollTop: $(this.hash).offset().top});
+
+        return false;
     });
 
     // 소스 넣기
     SyntaxHighlighter.highlight();
+
+    // 위로가기
+    $('.btn_top').on('click', function()
+    {
+        $('html, body').animate({scrollTop: 0});
+    });
 });
