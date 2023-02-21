4e713753d0459e003b35b55dc7e7083d1ce9bfd5
조민지 <iam924mj@plani.co.kr>
2023-02-21 10:05:53 +0900

    가이드 페이지 폴더 변경 및 기타 작업 (게시판 스타일가이드 진행 중)

---
+++ jquery/js/board.js
@@ -0,0 +1,116 @@
+/*-------------------------------------------------
+title       : 게시판
+Author      : PLAN I
+Create date : 2023-01-01
+-------------------------------------------------*/
+
+$(function()
+{
+    /*------------------------------------------------- 갤러리 -------------------------------------------------*/
+
+    // 선택자로 만들기 위한 클래스 추가
+    $('.gallery_view').find('ul').addClass('swiper');
+
+    // 이전 다음 버튼 추가
+    $('.gallery_view').append('<p class="control"><button class="prev">이전</button><button class="next">다음</button></p>');
+    
+    // 썸네일 슬라이더
+    var gallery_thumb = new Swiper(".gallery_view .thumb", 
+    {
+        wrapperClass        : "swiper",
+        slideClass          : "swiper > li",
+        slidesPerView       : 6,
+        spaceBetween        : 10,
+        loop                : false,
+    });
+
+    // 썸네일 링크 동작 안 하게
+    $('.gallery_view .thumb').find('a').on('click', function(){return false;});
+
+    // 이미지 슬라이더
+    var gallery_img = new Swiper(".gallery_view .list", 
+    {
+        wrapperClass        : "swiper",
+        slideClass          : "swiper > li",
+        loop                : true,
+        navigation          : 
+        {
+            nextEl  : ".gallery_view .next",
+            prevEl  : ".gallery_view .prev",
+        },
+        thumbs              : 
+        {
+            swiper  : gallery_thumb,
+        }
+    });
+
+    /*------------------------------------------------- //갤러리 -------------------------------------------------*/
+
+    // FAQ
+    $('.faq .label').on('click', function()
+    {
+        if ( $(this).parent('.group').hasClass('active') == false )
+        {
+            $(this).parent('.group').addClass('active');
+        }
+        else
+        {
+            $(this).parent('.group').removeClass('active');
+        }
+
+        return false;
+    });
+
+    // 댓글
+    $('.comment .list li').each(function(i)
+    {
+        $(this).find('.date').css({'left': $(this).find('.name').width()});
+    });
+
+    // 파일 첨부
+    $('.form_file').find('input').on('change', function()
+	{
+		if ( $(this).val() != '' )
+		{
+			$(this).next('.txt').addClass('active');
+			$(this).next('.txt').text('' + $(this).val() + '');
+		}
+	});
+
+    /*------------------------------------------------- 데이트피커 -------------------------------------------------*/
+
+    $.datepicker.setDefaults
+    ({
+        monthNamesShort     : ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNamesMin         : ['일', '월', '화', '수', '목', '금', '토'],
+        dateFormat          : 'yy/mm/dd',
+        showMonthAfterYear  : true,
+        changeYear          : true,
+        changeMonth         : true,
+        yearRange           : "c-100:c+10",
+        showOn              : 'both'
+    });
+
+    $('#view_sdate, #view_edate, #data01').datepicker
+    ({
+        buttonText: '<i class="ri-calendar-todo-fill"><span class="sr_only">달력 보기</span></i>'
+    });
+
+    /*------------------------------------------------- //데이트피커 -------------------------------------------------*/
+
+    // 게시판 목록 번호와 제목(.txt_left) 사이에 다른 셀이 존재할 때
+    $('.tstyle_list td').each(function()
+    {
+        if ( $(this).index() > 0 && $(this).next('td').hasClass('txt_left') == true )
+        {
+            if ( $(this).text() != '' )
+            {
+                $(this).parent('tr').addClass('type1');
+            }
+            else
+            {
+                $(this).addClass('hidden_m');
+            }
+        }
+    }); 
+});
---
+++ main/_guide/js/guide.js
@@ -0,0 +1,107 @@
+/*-------------------------------------------------
+title       : 가이드
+Author      : PLAN I
+Create date : 2023-01-01
+-------------------------------------------------*/
+
+$(function()
+{   
+    $('main > .section').each(function(i)
+    {
+        // 아이디값 넣기
+        $('main > .section').eq(i).attr('id', 'section' + i);
+
+        // 메뉴 생성
+        $('#gnb').append('<li><a href="#section' + i + '">' + $(this).find(' > h2').text() + '</a><ul class="depth2"></ul></li>');
+
+        // 마크업 한 번 넣어도 소스 저절로 생성되게
+        $('main > .section').eq(i).find('> .group').each(function(j)
+        {
+            $('main > .section').eq(i).find('> .group').eq(j).attr('id', 'group' + i + '_' + j);
+            $('#gnb').find('> li').eq(i).find('.depth2').append('<li><a href="#group' + i + '_' + j + '">' + $(this).find(' > h3').text() + '</a></li>');
+
+            $(this).find('> pre').text( $(this).find('> .item').html() );
+
+            // 소스 복사 버튼 생성
+            $(this).find('> .item').after('<textarea></textarea>');
+            $(this).find('> textarea').text( $(this).find('> .item').html() );
+            $(this).find('> .item').after('<button>소스 복사하기</button>');
+        });
+    });
+
+    $('main > .section > .group > .item').each(function(i)
+    {
+        // 스크롤
+        $(window).on('load scroll', function()
+        {
+            var sectionPosition = ($('main > .section > .group > .item').eq(i).offset() || { 'top': NaN }).top;
+
+            if ( $('html, body').scrollTop() >= (sectionPosition - 300) )
+            {
+                if ( $('#gnb .depth2').find('> li').eq(i).hasClass('active') == false )
+                {
+                    $('#gnb').find('li').removeClass('active');
+
+                    $('#gnb .depth2').find('> li').eq(i).parents('li').addClass('active');
+                    $('#gnb .depth2').find('> li').eq(i).addClass('active');
+                }
+            }
+        });
+    });
+
+    // 메뉴 눌렀을 때
+    $('#gnb').find('a').on('click', function()
+    {
+        $('html, body').animate({scrollTop: ($(this.hash).offset().top - 100)});
+
+        return false;
+    });
+
+    // 현재 메뉴 활성화
+    $('#lnb a').each(function(i)
+    {
+        if ( $(this).attr('href') == $(location).attr('pathname').match(/([^\/]*)\/*$/)[1] )
+        {
+            $(this).addClass('active');
+        }
+    });
+
+    $('.btn_control').on('click', function()
+    {
+        if ( $(this).hasClass('active') == false )
+        {
+            $(this).addClass('active');
+            $(this).text('전체 소스 열기');
+
+            $('main > .section > .group').find('> button').hide();
+            $('.syntaxhighlighter').addClass('disable');
+        }
+        else
+        {
+            $(this).removeClass('active');
+            $(this).text('전체 소스 닫기');
+
+            $('main > .section > .group').find('> show').hide();
+            $('.syntaxhighlighter').removeClass('disable');
+        }
+    });
+
+    // 소스 복사
+    $('main > .section > .group').find('> button').on('click', function()
+    {
+        $(this).siblings('textarea').select();
+
+        document.execCommand('Copy');
+
+        alert('소스가 복사되었어여');
+    });
+
+    // 소스 넣기
+    SyntaxHighlighter.highlight();
+
+    // 위로가기
+    $('footer button').on('click', function()
+    {
+        $('html, body').animate({scrollTop: 0});
+    });
+});
