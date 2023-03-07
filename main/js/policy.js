2829d69603c4cf3afa983a34f3833bf0caa812d5
leejihyun <leejihyun@plani.co.kr>
2023-03-07 15:20:26 +0900

    개인정보처리방침 수정

--- main/js/policy.js
+++ main/js/policy.js
@@ -3,29 +3,70 @@ title       : 가이드
 Author      : PLAN I
 Create date : 2023-01-01
 -------------------------------------------------*/
-
-$(function()
-{   
+$(document).ready(function () {
    
-
     //개인정보처리방침
     mouseOver($('.labeling .label_box'));
 
+    function Mobile(){return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);}
+    
+    
     function mouseOver(label) {
+        var device = $(window).width();
+        if (Mobile()) { // 모바일일 경우
 
-        label.focus(function(){
-            $(this).parent('li').addClass('active');
-        });
-        label.blur(function(){
-            $(this).parent('li').removeClass('active');
-        });
-        label.on("mouseover focus", function() {
-            $(this).parent('li').addClass('active');
-        });
-        label.on("mouseout focusout", function() {
-            $(this).parent('li').removeClass('active');
-        });
+            label.on("click", function () {
+                if ($(this).parent('li').hasClass('active')) {
+                    $('.labeling .label_box').parent('li').removeClass('active');
+                } else {
+                    $(this).parent('li').addClass('active');
+                }
+            });
+            
+        } else { // 모바일 외
+
+            label.focus(function () {
+                $(this).parent('li').addClass('active');
+            });
+            label.blur(function () {
+                $(this).parent('li').removeClass('active');
+            });
+            label.on("mouseover focus", function () {
+                $(this).parent('li').addClass('active');
+            });
+            label.on("mouseout focusout", function () {
+                $(this).parent('li').removeClass('active');
+            });
+        }
     }
+    
+    // function mouseOver(label) {
+    //     var device = $(window).width();
+
+    //     if(device >= 1024) {
+
+    //         label.focus(function(){
+    //             $(this).parent('li').addClass('active');
+    //         });
+    //         label.blur(function(){
+    //             $(this).parent('li').removeClass('active');
+    //         });
+    //         label.on("mouseover focus", function() {
+    //             $(this).parent('li').addClass('active');
+    //         });
+    //         label.on("mouseout focusout", function() {
+    //             $(this).parent('li').removeClass('active');
+    //         });
+    //     }else{
+    //         label.on("click", function() {
+    //             if($(this).parent('li').hasClass('active')){
+    //                 $('.labeling .label_box').parent('li').removeClass('active');
+    //             }else{
+    //                 $(this).parent('li').addClass('active');
+    //             }
+    //         });
+    //     }
+    // }
 
     $('.labeling_list a').on('click', function(){
         if($(this).parent('li').hasClass('active')){
