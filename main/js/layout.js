131d1d905aefbbc406321292658b352fb68badb6
조민지 <iam924mj@plani.co.kr>
2023-03-22 11:03:17 +0900

    메인 반응형

--- main/js/layout.js
+++ main/js/layout.js
@@ -44,13 +44,20 @@ $(function()
         wrapperClass            : "list",
         slideClass              : "list > li",
         slidesPerView           : "auto",
-        spaceBetween            : 25,
+        spaceBetween            : 10,
         loop                    : false,
         loopAdditionalSlides    : 1,            // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
         navigation              : 
         {
             nextEl              : ".footbanner .next",
             prevEl              : ".footbanner .prev",
+        },
+        breakpoints             : 
+        {
+            769 : 
+            {
+                spaceBetween    : 25,
+            },
         }
     });
 
